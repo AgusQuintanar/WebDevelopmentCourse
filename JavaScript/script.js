81f4d0ae578a76026b3a4a23353430a737f252f7
AgusQuintanar <agusquintanar17@gmail.com>
2019-05-27 20:10:18 -0500

    added functions examples

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,38 @@
-4 + 3;
+function teoria() {
 
-alert("hola mundo")
+    //Imprimir en consola
+    console.log("print en java");
+    console.log("hola mundo");
+
+    // Input
+    prompt("Ingrese su nombre: ");
+
+    // Alertas
+    alert("Esto es una alerta");
+    alert("hola mundo");
+
+}
+
+
+
+// Funciones
+
+function sayHello() {
+    console.log("Hello");
+}
+//sayHello();
+
+// Funciones anonimas
+
+var sayBye = function() {
+    console.log("Bye");
+}
+
+//sayBye();
+
+
+function multiplicar(a,b) {
+    return a*b;
+}
+
+ console.log(multiplicar(6,7)); 
